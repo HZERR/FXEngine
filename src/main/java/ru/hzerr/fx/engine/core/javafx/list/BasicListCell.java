b1539808586607b14ccd616d27d6e90a08ea458d
HZERR <vadimvyazloy@yandex.ru>
2024-01-21 16:56:58 +0300

    Fix BasicListCell

--- src/main/java/ru/hzerr/fx/engine/core/javafx/list/BasicListCell.java
+++ src/main/java/ru/hzerr/fx/engine/core/javafx/list/BasicListCell.java
@@ -9,6 +9,8 @@ public abstract class BasicListCell<T> extends ListCell<T> {
     @Override
     protected void updateItem(T item, boolean empty) {
         super.updateItem(item, empty);
+
+        onUpdateItem(item, empty);
     }
 
     public abstract void onUpdateItem(T item, boolean empty);
